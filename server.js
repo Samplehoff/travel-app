e429928f4f666ac5fb43a77d741969dfa8ea2916
Sampey Hoffman <53267153+Samplehoff@users.noreply.github.com>
2020-02-20 18:19:03 -0600

    updated

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require("express");
+const express = require("express");
+const app = express();
 
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT, function () {
     console.log('server listening on port ' + process.env.PORT + ' app name= ' + process.env.PROJECT_NAME);
