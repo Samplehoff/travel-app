1be0854489485fa6cb9129c16ff5ba303f519b2d
Sampey Hoffman <53267153+Samplehoff@users.noreply.github.com>
2020-02-26 16:08:49 -0600

    node

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(3004, function (){
+    console.log("listening on server 3004")
+})
